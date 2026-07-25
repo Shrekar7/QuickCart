ec5389cfc467cedb38f6d92d330e624a1dc1481f
shrekar <shrekarreddy7@gmail.com>
2026-07-25 23:22:11 +0530

    fix: make syncUserCreation idempotent

--- config/inngest.js
+++ config/inngest.js
@@ -3,6 +3,7 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
 import User from "@/models/User";
+import Order from "@/models/Order";
 
 export const inngest = new Inngest({
   id: "Sunshine-next",
@@ -33,7 +34,11 @@ export const syncUserCreation = inngest.createFunction(
     };
 
     await connectDB();
-    await User.create(userData);
+    await User.findByIdAndUpdate(id, userData, {
+      upsert: true,
+      new: true,
+      setDefaultsOnInsert: true,
+    });
   }
 );
 
@@ -81,3 +86,34 @@ export const syncUserDeletion = inngest.createFunction(
     await User.findByIdAndDelete(id);
   }
 );
+
+// function to create the user's order in database
+
+export const createUserOrder = inngest.createFunction(
+    {
+        id:'create-user-order',
+        batchEvents: {
+            maxSize: 25, 
+            timeout: '5s'
+        }
+    },
+    {event: 'order/created'},
+    async ({events}) => {
+
+        const orders = events.map((event)=> {
+            return{
+                userId: event.data.userId,
+                items: event.data.items,
+                amount: event.data.amount,
+                address: event.data.address,
+                date : event.data.date
+            }
+        })
+
+        await connectDB()
+        await Order.insertMany(orders)
+
+        return {success: true, processed: orders.length};
+
+    }
+)
